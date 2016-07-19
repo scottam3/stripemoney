5011bec6cc9ec22d39c8cb87b5c0e8a00e72d403
scott murtaugh <scott.murtaugh@gmail.com>
2016-07-18 22:26:05 -0400

    seeded product table

--- db/migrate/20160719020942_create_products.rb
+++ db/migrate/20160719020942_create_products.rb
@@ -9,6 +9,11 @@ class CreateProducts < ActiveRecord::Migration
       t.string :sku
       t.decimal :price
       t.timestamps null: false
+      t.string :length
+      t.string :author_image_name
+      t.string :download_url
+      t.string :details
+      t.text :author_description
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,13 @@ ActiveRecord::Schema.define(version: 20160719020942) do
     t.text     "description"
     t.string   "sku"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "length"
+    t.string   "author_image_name"
+    t.string   "download_url"
+    t.string   "details"
+    t.text     "author_description"
   end
 
   create_table "purchases", force: :cascade do |t|
